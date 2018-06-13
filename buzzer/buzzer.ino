a0a9d196050e510d8d77a99a5502e78deb446267
kura10417 <kura10417@gmail.com>
2018-06-13 10:21:27 +0800

    Add files via upload

---
+++ buzzer/buzzer.ino
@@ -0,0 +1,32 @@
+int speakerPin = 8;
+char notes[] = "ccggaagffeeddc ";
+unsigned long beats[] = {1,1,1,1,1,1,2,1,1,1,1,1,1,2,4}; 
+int length = sizeof(notes); 
+int tempo = 300;
+void setup() {
+  pinMode(speakerPin, OUTPUT);
+}
+
+void loop() {
+
+  for (int i = 0; i < length; i++) {
+    if (notes[i] == ' ') {
+      delay(beats[i] * tempo); 
+    } else {
+      playNote(speakerPin,notes[i], beats[i] * tempo);
+    } 
+    delay(tempo/10); 
+  }
+}
+
+void playNote(int OutputPin, char note, unsigned long duration) {
+  char names[] = { 'c', 'd', 'e', 'f', 'g', 'a', 'b', 'C' };
+  int tones[] = { 261, 294, 330, 349, 392, 440, 494, 523 };
+  for (int i = 0; i < 8; i++) {
+    if (names[i] == note) {
+      tone(OutputPin,tones[i], duration);
+      delay(duration);
+      noTone(OutputPin);
+    }
+  }
+}
